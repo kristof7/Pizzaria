5929f69809e124a38526f016fcf641a3e4e62c2d
Krzysztof Kampa <krzysztofk7777@gmail.com>
2020-02-17 22:45:06 +0100

    Initial commit

---
+++ pizzariaApp/Order.java
@@ -0,0 +1,30 @@
+public class Order {
+
+    private String client;
+    private String address;
+    private int phoneNumber;
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
